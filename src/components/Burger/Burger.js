fcb535d269e3194b1cbacd2fd962373090fccdfc
dzenvikas <vikaskumar.vsk@gmail.com>
2019-04-15 02:08:56 +0530

    ingredients updates dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,22 @@ import styles from './Burger.module.css';
 import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return [...Array(props.ingredients[ingKey])].map((_, index) => {
+        return <BurgerIngredient key={ingKey + index} type={ingKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    });
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add some ingredients!</p>;
+  }
   return (
     <div className={styles.burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="patty" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 2,
+      cheese: 2,
+      patty: 1
+    }
+  };
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger controls</div>
       </Aux>
     );
