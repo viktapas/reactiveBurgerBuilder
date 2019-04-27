dea84cdd609636274e288cec88c1f33b95c07b35
dzenvikas <vikaskumar.vsk@gmail.com>
2019-04-27 09:00:35 +0530

    bug fix

--- src/components/Navingation/Toolbar/DrawerToggle/DrawerToggle.js
+++ src/components/Navingation/Toolbar/DrawerToggle/DrawerToggle.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import styles from './DrawerToggle.module.css';
 
-const DrawerToggle = props => <div onClick={props.clicked}>MENU</div>;
+const DrawerToggle = props => (
+  <div className={styles.drawerToggle} onClick={props.clicked}>
+    <div />
+    <div />
+    <div />
+  </div>
+);
 
 export default DrawerToggle;
