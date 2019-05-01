adf1b4a7d4cbf2e99bee92037383877d92e71a1e
dzenvikas <vikaskumar.vsk@gmail.com>
2019-05-01 16:25:20 +0530

    Orders, order components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from './hoc/Layout/Layout';
 import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch } from "react-router-dom";
+import Orders from "./containers/Orders/Orders";
 
 class App extends Component {
   render() {
     return (
       <Layout>
         <Switch>
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/" component={BurgerBuilder} />
-
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/" component={BurgerBuilder} />
         </Switch>
       </Layout>
     );
---
+++ src/components/Order/Order.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Order = (props) => (
+    <div>
+        <p>Ingredients: Salad</p>
+        <p>Price: <strong>INR 35</strong></p>
+    </div>
+);
+
+export default Order;
